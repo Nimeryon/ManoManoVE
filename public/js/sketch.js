45d25bc8c3d1ad672f4718e7478884a4e26194d6
Nimeryon <t.auriol46@gmail.com>
2021-02-11 23:09:10 +0100

    Le Schnaps

--- public/js/sketch.js
+++ public/js/sketch.js
@@ -1,53 +1,55 @@
-var canvas_width = 800;
-var canvas_height = 600;
+if (window.location.pathname == "/") {
+    var canvas_width = 800;
+    var canvas_height = 600;
 
-var room;
-var furnitures;
+    var room;
+    var furnitures;
 
-function setup() {
-    let canvas = createCanvas(800, 600);
-    canvas.parent(document.getElementById("canvas"));
+    function setup() {
+        let canvas = createCanvas(800, 600);
+        canvas.parent(document.getElementById("canvas"));
 
-    room = new Room(5, 5, canvas_width, canvas_height);
-    room.furnitures = [];
-}
-
-function draw() {
-    background(255);
-
-    for (var i = 0; i < room.furnitures.length; i++) {
-        room.furnitures[i].draw();
+        room = new Room(5, 5, canvas_width, canvas_height);
+        room.furnitures = [];
     }
-    room.draw();
-}
 
-function mousePressed() {
-    for (var i = room.furnitures.length - 1; i >= 0; i--) {
-        if (room.furnitures[i].mouseIn(mouseX, mouseY)) {
-            room.clearSelection();
-            room.furnitures[i].selected = true;
-            room.selection = i;
-            return;
-        } else {
-            room.clearSelection();
+    function draw() {
+        background(255);
+
+        for (var i = 0; i < room.furnitures.length; i++) {
+            room.furnitures[i].draw();
+        }
+        room.draw();
+    }
+
+    function mousePressed() {
+        for (var i = room.furnitures.length - 1; i >= 0; i--) {
+            if (room.furnitures[i].mouseIn(mouseX, mouseY)) {
+                room.clearSelection();
+                room.furnitures[i].selected = true;
+                room.selection = i;
+                return;
+            } else {
+                room.clearSelection();
+            }
         }
     }
-}
 
-function mouseDragged() {
-    if (room.selection != null) {
-        room.furnitures[room.selection].move(mouseX, mouseY, room);
-    }
-}
-
-function keyPressed() {
-    if (room.selection != null) {
-        if (keyCode == RIGHT_ARROW) {
-            room.furnitures[room.selection].rotate();
+    function mouseDragged() {
+        if (room.selection != null) {
+            room.furnitures[room.selection].move(mouseX, mouseY, room);
         }
-        if (keyCode == BACKSPACE) {
-            room.furnitures.splice(room.selection, 1);
-            room.selection = null;
+    }
+
+    function keyPressed() {
+        if (room.selection != null) {
+            if (keyCode == RIGHT_ARROW) {
+                room.furnitures[room.selection].rotate();
+            }
+            if (keyCode == BACKSPACE) {
+                room.furnitures.splice(room.selection, 1);
+                room.selection = null;
+            }
         }
     }
 }
